2d331b23a6e191a0a4a2810a01b87f9bf00efd3c
Abhay Deshpande <a.deshpande012@gmail.com>
2024-05-02 15:48:57 -0700

    More config changes

--- diffusion_policy/configs.py
+++ diffusion_policy/configs.py
@@ -46,18 +46,21 @@ class DatasetConfig:
     # SERL type is the data from record_demos, HDF5 is the type outputed from robomimic, Jacob is the
     # data from process_data.py
     type: str = "D4RL"  # Options are SERL or HDF5 or Jacob or D4RL
-    dataset_path: str = "${hydra:runtime.cwd}/data/piggy-bank/piggy_bank_75_corr.pkl" # Path to your training datset
+    # dataset_path: str = "${hydra:runtime.cwd}/data/piggy-bank/piggy_bank_75_corr.pkl"
+    # dataset_path: str = "${hydra:runtime.cwd}/data/grasp_cube_100_aug.pkl"
+    dataset_path: str = "${hydra:runtime.cwd}/data/lego_filtered.pkl"
+    # dataset_path: str = "${hydra:runtime.cwd}/data/coin_filtered.pkl"
     num_traj: int = -1 # Number of trajectories to train on. -1 is all of them
 
     # The keys from observations that we use for the inputs to our model
     image_keys: list = field(default_factory=lambda: [])
 
 @dataclass
-class DiffusionModelRunConfig:
+class DiffusionModelRunConfig_Cube:
     hydra: ExperimentHydraConfig = ExperimentHydraConfig()
     dataset: DatasetConfig = DatasetConfig()
     device: str = "cuda"
-    checkpoint_path: str = "${hydra:runtime.cwd}/chkpt.pt"
+    checkpoint_path: str = "${hydra:runtime.cwd}/graspcube_100.pt"
 
     batch_size: int = 256//2
     num_epochs: int = 16
@@ -65,7 +68,7 @@ class DiffusionModelRunConfig:
     # If with_state, uses the state keys. If without doesn't and state len does not matter
     with_state: bool = True
     # Length of the concatenated state
-    state_len: int = 10
+    state_len: int = 8
     with_image: bool = False
     # Number of images in the observation. Should be equal to the length of image_keys
     num_cameras: int = 0
@@ -77,7 +80,57 @@ class DiffusionModelRunConfig:
     num_diffusion_iters: int = 100
     num_eval_diffusion_iters: int = 16
 
+@dataclass
+class DiffusionModelRunConfig_Lego:
+    hydra: ExperimentHydraConfig = ExperimentHydraConfig()
+    dataset: DatasetConfig = DatasetConfig()
+    device: str = "cuda"
+    checkpoint_path: str = "${hydra:runtime.cwd}/lego_100.pt"
 
+    batch_size: int = 256//2
+    num_epochs: int = 16
+
+    # If with_state, uses the state keys. If without doesn't and state len does not matter
+    with_state: bool = True
+    # Length of the concatenated state
+    state_len: int = 7
+    with_image: bool = False
+    # Number of images in the observation. Should be equal to the length of image_keys
+    num_cameras: int = 0
+
+    action_dim: int = 4
+    pred_horizon: int = 12
+    obs_horizon: int = 4
+    action_horizon: int = 8
+    num_diffusion_iters: int = 100
+    num_eval_diffusion_iters: int = 16
+
+@dataclass
+class DiffusionModelRunConfig_Coin:
+    hydra: ExperimentHydraConfig = ExperimentHydraConfig()
+    dataset: DatasetConfig = DatasetConfig()
+    device: str = "cuda"
+    checkpoint_path: str = "${hydra:runtime.cwd}/coin_100.pt"
+
+    batch_size: int = 256//2
+    num_epochs: int = 16
+
+    # If with_state, uses the state keys. If without doesn't and state len does not matter
+    with_state: bool = True
+    # Length of the concatenated state
+    state_len: int = 8
+    with_image: bool = False
+    # Number of images in the observation. Should be equal to the length of image_keys
+    num_cameras: int = 0
+
+    action_dim: int = 5
+    pred_horizon: int = 12
+    obs_horizon: int = 4
+    action_horizon: int = 8
+    num_diffusion_iters: int = 100
+    num_eval_diffusion_iters: int = 16
+
+DiffusionModelRunConfig = DiffusionModelRunConfig_Lego
 
 
 
